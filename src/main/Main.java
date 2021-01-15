f2e506fbbc264e7691c0c80891b9a8d44f5e70c5
Ale Panozo <panozoaler.47@gmail.com>
2021-01-15 13:55:19 -0300

    quite el for del main y lo pase a la clase Alumnas

--- src/main/Main.java
+++ src/main/Main.java
@@ -10,22 +10,15 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         IPoblarClases poblarClases = new PoblarClasesImpl();
-        double notaFinal;
+
         List <Alumnas> alumnas = poblarClases.CrearAlumnas();
         //System.out.println(alumnas);
+        //System.out.print("mensaje de prueba para subir a git");
+        //System.out.print("segundaaaa prueba para subir a git");
 
-        for (Alumnas alumnaiteradora : alumnas){
-            notaFinal = alumnaiteradora.CalcularNotaFinal();
-            if (notaFinal >= 4.0){
-                System.out.println(" Las alumnas aprobadas son " + alumnaiteradora.getNombre() + " " + alumnaiteradora.getApellido() +" con "+ notaFinal );
-            }
-            else {
-                System.out.println("Las alumnas reprobadas son " + alumnaiteradora.getNombre() + " " + alumnaiteradora.getApellido() +" con "+ notaFinal );
-            }
-            //System.out.print("mensaje de prueba para subir a git");
-            //System.out.print("segundaaaa prueba para subir a git");
-        }
         //System.out.println("Esto es un ensayo");
+
+        System.out.printf();
 
         List<Curso> cursos = poblarClases.CrearCursos();
         System.out.println(cursos);
--- src/modelos/Alumnas.java
+++ src/modelos/Alumnas.java
@@ -2,9 +2,13 @@ package modelos;
 
 import modelos.enums.TipoCurso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Alumnas extends  Persona {
 
     private Notas Notas;
+    private double notaFinal;
 
 
     public Alumnas(String nombre, String apellido, int dni, modelos.Notas notas) {
@@ -33,5 +37,19 @@ public class Alumnas extends  Persona {
         notas = getNotas();
         NotaFinal = notas.getNota1()*0.1 + notas.getNota2()*0.2 + notas.getNota3()*0.3 + notas.getNota4()*0.4 ;
         return NotaFinal;
+    }
+
+    public double AlumnasAprobadas() {
+        List<Alumnas> alumnas = new ArrayList<>();
+
+        for (Alumnas alumnaiteradora : alumnas) {
+            notaFinal = alumnaiteradora.CalcularNotaFinal();
+            if (notaFinal >= 4.0) {
+                System.out.println(" Las alumnas aprobadas son " + alumnaiteradora.getNombre() + " " + alumnaiteradora.getApellido() + " con " + notaFinal);
+            } else {
+                System.out.println("Las alumnas reprobadas son " + alumnaiteradora.getNombre() + " " + alumnaiteradora.getApellido() + " con " + notaFinal);
+            }
+        }
+        return notaFinal;
     }
 }
